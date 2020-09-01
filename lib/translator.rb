abbb332d9a2c8e3f4635846e509d828b2d4bad45
Marisa You <marisa.you@gmail.com>
2020-09-01 21:21:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,21 @@
 require "yaml"
 
 def load_library(file)
-  emoticons = YAML.load_file(file)
+  return YAML.load_file(file)
 end
 
-def get_japanese_emoticon(file, )
+def get_japanese_emoticon(file, emoticon)
   # code goes here
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+  dict = load_library(file)
+  dict.each do |key, value|
+    for i in value
+      if i == emoticon
+        return key
+      end
+    end
+  end
+      
 end
