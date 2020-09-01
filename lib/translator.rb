56af5da8dd3ef26c74172575c21b84e87552ca0c
Marisa You <marisa.you@gmail.com>
2020-09-01 21:32:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ def get_japanese_emoticon(file, emoticon)
     end
   end
   
-  return "Sorry, that emoticon was not found
+  return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(file, emoticon)
